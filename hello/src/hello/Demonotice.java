2623f8f68ea82e46dbbca014790964ec86ababaf
Peter <dydx2000.py@gmail.com>
2019-05-19 22:16:13 +0800

    not

---
+++ hello/src/hello/Demonotice.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Demonotice {
+    public static void main(String[] args) {
+        byte num1 = 30;
+        System.out.println(num1);
+        char zifu = (char)65;
+        System.out.println(zifu);
+    }
+}
