8c8a0c0e8449f49632f86adb3d7fbcabbbdd89af
Peter <dydx2000.py@gmail.com>
2019-05-20 00:30:03 +0800

    for

---
+++ day05/src/hellojava.java
@@ -0,0 +1,23 @@
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+        System.out.println(add(9,10));
+        add(10,90);
+        int j = add(32,3234);
+        System.out.println(j);
+
+    }
+    public static int add(int x,int y){
+        int result = x +y;
+//        System.out.println(result);
+        return result;
+
+    }
+
+
+}
